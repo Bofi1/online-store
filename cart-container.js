f592633ca3cb7e662a07ae8afba0ddcbae2c7766
Bofi1 <angelborque03github@gmail.com>
2024-09-10 19:05:55 -0700

    addCartDiv & ItemOnFile update

--- cart-container.js
+++ cart-container.js
@@ -1,10 +1,9 @@
 
 
-// ---- cart-container
+// ---- animacion al carrito
 let cartContainer = document.getElementById("cartContainer")
 let cartHiddenButton = document.getElementById("close-cart-icon")
 let cartVisibleButton = document.getElementById("header-cart-icon")
-// let cartStatus = true
 
 cartHiddenButton.addEventListener("click", cartHidden)
 cartVisibleButton.addEventListener("click", cartVisible)
@@ -24,27 +23,78 @@ function cartVisible() {
 //  ----------------
 
 
+//---- le damos funcionalidad al boton .product-addCartButton
 let productAddCartButton = document.getElementsByClassName("product-addCartButton")
 
 for (let i = 0; i < productAddCartButton.length; i++) {
     let button = productAddCartButton[i]
     button.addEventListener("click", addToCart)
 }
+// ---------------------
 
+
+
+// --- tomamos la informacion del producto que se seleccione
 function addToCart(event) {
     let button = event.target
     let selector = button.parentElement
+
+
     let tittle = selector.getElementsByClassName("product-tittle")[0]
-    console.log(tittle.innerHTML);
+    tittle = tittle.innerHTML;
+
     let price = selector.getElementsByClassName("product-price")[0]
     price = price.innerHTML.replace("$","").replace(",","").replace(".","")
-    console.log(price);
+    
     let img = selector.getElementsByClassName("product-img")[0].src
-    console.log(img);
     
+    ItemOnFile(tittle)
+
+    addCartDiv(img,tittle,price)
+
+    cartVisible()
+}
+
+
+
+// ---- añadimos el producto(.product-item) al carrito(.cart-items-container) como item(.cart-item)
+function addCartDiv(img,tittle,price) {
+    if (isItemOnFile == false) {
+        let cartItemsContainer = document.getElementsByClassName("cart-items-container")[0]
+        let div = document.createElement("div")
+        div.classList.add("cart-item")
+        let cartItem = cartItemsContainer.appendChild(div)
+        cartItem.innerHTML = `
+            <img src="${img}" alt="">
+            <span class="cart-item-tittle">${tittle}</span>
+            <span class="cart-item-price">${price}</span>
     
+            <div class="cart-item-barra">
+                <div class="minus">-</div>
+                <div class="plus">+</div>
+            </div>
+        `
+    } else {alert("repetido")}
+}
+
+
+// ---- comprobar que cart-item no se repita
+let isItemOnFile = false
+function ItemOnFile(tittle) {
+    let cartItem = document.getElementsByClassName("cart-item")
+
+    for (let i = 0; i < cartItem.length; i++) {
+        let findName = cartItem[i].getElementsByClassName("cart-item-tittle")[0].innerHTML
+        
+        if (findName == tittle) {
+            isItemOnFile = true
+        } 
+        
+    }
     
 }
+
+
 
 
 
